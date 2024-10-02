ef43ed3a98ddecedb4a391943fa448f7ea8898b6
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-10-02 12:20:30 -0500

    update view goals page to use use pages function call

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,52 +1,66 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Box, Card, Button, List, ListItem } from "@mui/material";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import Results from "./Results";
 import "../css/Goals.css";
 
 const GoalsList = ({ goals }) => {
-  const [goalToShow, setGoalToShow] = useState(null);
+  const [result, setResults] = useState(null);
   const handleShowGoal = async (goalId) => {
     console.log("Goal ID", goalId);
     try {
       const token = localStorage.getItem("authToken");
-      const response = await axios.get(`/api/goals/${goalId}`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+      const response = await axios.post(
+        `/api/goal`,
+        {
+          goalId,
         },
-      });
-      console.log("Goal response", response.data);
-      setGoalToShow(response.data);
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setResults(response.data.goal.plan);
     } catch (error) {
       console.error("Error fetching goal:", error);
     }
   };
 
   return (
-    <div className="goals-list">
-      <h2>My Goals - Coming Soon</h2>
-      <ul>
-        {goals.length > 0 ? (
-          goals.map((goal, index) => (
-            <li
-              onClick={() => handleShowGoal(goal.GoalId)}
-              key={index}
-              className="goal-item"
-            >
-              {goal.goal_name}
-            </li>
-          ))
-        ) : (
-          <p>No goals available</p>
-        )}
-      </ul>
-      {goalToShow && (
-        <div className="goal-details">
-          <h2>{goalToShow.goal_name}</h2>
-          <p>{goalToShow.prompt}</p>
-          <p>{goalToShow.timeline}</p>
-        </div>
+    <Box className="goals-list">
+      {result ? (
+        <Box>
+          <Box style={{ display: "flex", justifyContent: "center" }}>
+            <Button onClick={() => setResults(null)} variant="outlined">
+              View All Goals
+            </Button>
+          </Box>
+          <Results result={result} />
+        </Box>
+      ) : (
+        <Card style={{ padding: "24px" }}>
+          <h2>My Goals</h2>
+          <List>
+            {goals.length > 0 ? (
+              goals.map((goal, index) => (
+                <ListItem
+                  onClick={() => handleShowGoal(goal.GoalId)}
+                  key={index}
+                  className="goal-item"
+                >
+                  {goal.goal_name}
+                </ListItem>
+              ))
+            ) : (
+              <p>No goals available</p>
+            )}
+          </List>
+        </Card>
       )}
-    </div>
+    </Box>
   );
 };
 
