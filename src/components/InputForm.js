5a90ed52ab2d0090f131cac2ea4db58a081b1b68
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-10-02 09:43:39 -0500

    refactor InputForm to use Mui components

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
+import {
+  Box,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Button,
+  TextField,
+  FormGroup,
+} from "@mui/material";
 
 const InputForm = ({ onSubmit, loading }) => {
   const [goal, setGoal] = useState("");
@@ -12,50 +22,68 @@ const InputForm = ({ onSubmit, loading }) => {
   };
 
   return (
-    <div className="input-form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-group">
-          <input
-            type="text"
-            placeholder="Type your goal..."
-            value={goal}
-            onChange={(e) => setGoal(e.target.value)}
-            className="form-input"
-            required
-          />
-        </div>
-        <div className="input-group">
-          <textarea
-            placeholder="Areas of focus..."
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
-            className="form-textarea"
-          />
-        </div>
-        <div className="input-group">
-          <select
-            value={timeline}
-            onChange={(e) => setTimeline(e.target.value)}
-            className="form-select"
-            required
+    <Box className="input-form">
+      <FormGroup>
+        <Box className="input-group">
+          <FormControl fullWidth>
+            <TextField
+              placeholder="Type your goal..."
+              value={goal}
+              onChange={(e) => setGoal(e.target.value)}
+              className="form-input"
+              required
+            />
+          </FormControl>
+        </Box>
+        <Box className="input-group">
+          <FormControl fullWidth>
+            <TextField
+              placeholder="Areas of focus..."
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+              className="form-textarea"
+              variant="outlined"
+              multiline
+              rows={3}
+            />
+          </FormControl>
+        </Box>
+        <Box className="input-group">
+          <FormControl fullWidth>
+            <InputLabel id="timeline-select-label">Timeline</InputLabel>
+            <Select
+              labelId="timeline-select-label"
+              className="form-select"
+              label="Timeline"
+              value={timeline}
+              onChange={(e) => setTimeline(e.target.value)}
+              required
+            >
+              <MenuItem value="" disabled>
+                Select Timeline...
+              </MenuItem>
+              <MenuItem value="1 day">1 Day</MenuItem>
+              <MenuItem value="1 week">1 Week</MenuItem>
+              <MenuItem value="1 month">1 Month</MenuItem>
+              <MenuItem value="3 months">3 Months</MenuItem>
+              <MenuItem value="6 months">6 Months</MenuItem>
+              <MenuItem value="1 year">1 Year</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        <Box style={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            type="submit"
+            variant={"contained"}
+            className="primary-button"
+            disabled={loading}
+            onClick={handleSubmit}
           >
-            <option value="" disabled>
-              Select Timeline...
-            </option>
-            <option value="1 day">1 Day</option>
-            <option value="1 week">1 Week</option>
-            <option value="1 month">1 Month</option>
-            <option value="3 months">3 Months</option>
-            <option value="6 months">6 Months</option>
-            <option value="1 year">1 Year</option>
-          </select>
-        </div>
-
-        <button type="submit" className="primary-button" disabled={loading}>
-          {loading ? <CircularProgress /> : "Show me the way"}
-        </button>
-      </form>
-    </div>
+            {loading ? <CircularProgress /> : "Show me the way"}
+          </Button>
+        </Box>
+      </FormGroup>
+    </Box>
   );
 };
 
